9993113056326778e1441f55717b4baf74ed278a
Bin Giulio Liu <bingiulio.liu@gmail.com>
2026-03-23 18:12:32 +0100

    FIXED esercizio, avevo capito malissimo

--- script/script.js
+++ script/script.js
@@ -4,50 +4,63 @@ const scontoOver = 0.6; // Sconto del 40%
 const passeggeroA = 10;
 const passeggeroB = 70;
 
-console.log ('Passeggero A minorenne');
-if (passeggeroA < 18) {
-    console.log( (prezzoAlKm * 100) * scontoUnder );
-}   else if (passeggeroA >= 65) {
-    console.log( (prezzoAlKm * 100 ) * scontoOver );
-}   else {
-    console.log ( (prezzoAlKm * 100 ) )
-}
+// console.log ('Passeggero A minorenne');
+// if (passeggeroA < 18) {
+//     console.log( (prezzoAlKm * 100) * scontoUnder );
+// }   else if (passeggeroA >= 65) {
+//     console.log( (prezzoAlKm * 100 ) * scontoOver );
+// }   else {
+//     console.log ( (prezzoAlKm * 100 ) )
+// }
 
-console.log ('Passeggero B over 65');
-if (passeggeroB < 18) {
-    console.log( (prezzoAlKm * 100) * scontoUnder );
-}   else if (passeggeroB >= 65) {
-    console.log( (prezzoAlKm * 100 ) * scontoOver );
-}   else {
-    console.log ( (prezzoAlKm * 100 ) )
-}
-
-
-console.log ('Randomiser di età');
-const anniViaggiatore = (Math.floor(Math.random() * 100) + 1 ); 
-console.log (anniViaggiatore); // randomiser età viaggiatore
-
-if (anniViaggiatore < 18) {
-    console.log( (prezzoAlKm * 100) * scontoUnder );
-}   else if (anniViaggiatore >= 65) {
-    console.log( (prezzoAlKm * 100 ) * scontoOver );
-}   else {
-    console.log ( (prezzoAlKm * 100 ) )
-}
+// console.log ('Passeggero B over 65');
+// if (passeggeroB < 18) {
+//     console.log( (prezzoAlKm * 100) * scontoUnder );
+// }   else if (passeggeroB >= 65) {
+//     console.log( (prezzoAlKm * 100 ) * scontoOver );
+// }   else {
+//     console.log ( (prezzoAlKm * 100 ) )
+// }
 
 
-console.log ('Randomiser di età e km');
-console.log (anniViaggiatore); // randomiser età viaggiatore
-const percorrenza = (Math.floor(Math.random() * 1000) + 1 ); 
-console.log (percorrenza); // randomiser km
+// console.log ('Randomiser di età');
+// const anniViaggiatore = (Math.floor(Math.random() * 100) + 1 ); 
+// console.log (anniViaggiatore); // randomiser età viaggiatore
 
-if (anniViaggiatore < 18) {
-    let totaleRandom = ( (prezzoAlKm * percorrenza) * scontoUnder );
-    console.log(`€${totaleRandom.toFixed(2)}`);
-}   else if  (anniViaggiatore >= 65) {
-    let totaleRandom = ( (prezzoAlKm * percorrenza) * scontoOver );
-    console.log(`€${totaleRandom.toFixed(2)}`);
-}   else {
-    let totaleRandom = ( (prezzoAlKm * percorrenza ) )
-    console.log(`€${totaleRandom.toFixed(2)}`);
-}
+// if (anniViaggiatore < 18) {
+//     console.log( (prezzoAlKm * 100) * scontoUnder );
+// }   else if (anniViaggiatore >= 65) {
+//     console.log( (prezzoAlKm * 100 ) * scontoOver );
+// }   else {
+//     console.log ( (prezzoAlKm * 100 ) )
+// }
+
+
+// console.log ('Randomiser di età e km');
+// console.log (anniViaggiatore); // randomiser età viaggiatore
+// const percorrenza = (Math.floor(Math.random() * 1000) + 1 ); 
+// console.log (percorrenza); // randomiser km
+
+// if (anniViaggiatore < 18) {
+//     let totaleRandom = ( (prezzoAlKm * percorrenza) * scontoUnder );
+//     console.log(`€${totaleRandom.toFixed(2)}`);
+// }   else if  (anniViaggiatore >= 65) {
+//     let totaleRandom = ( (prezzoAlKm * percorrenza) * scontoOver );
+//     console.log(`€${totaleRandom.toFixed(2)}`);
+// }   else {
+//     let totaleRandom = ( (prezzoAlKm * percorrenza ) )
+//     console.log(`€${totaleRandom.toFixed(2)}`);
+// }
+
+let anniPasseggero = Number(prompt('Quanti anni hai?'));
+let prezzoBiglietto;
+
+if (anniPasseggero < 18) {
+    prezzoBiglietto = ( (prezzoAlKm * 100) * scontoUnder )
+    }   else if (anniPasseggero >= 65) {
+    prezzoBiglietto = ( (prezzoAlKm * 100 ) * scontoOver );
+    }   else {
+    prezzoBiglietto = ( (prezzoAlKm * 100 ) )
+    }
+
+alert(`Il prezzo del biglietto è di €${prezzoBiglietto.toFixed(2)}`);
